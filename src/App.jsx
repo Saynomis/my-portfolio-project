f218f8ec4452503d175109baedb56eb6772f6ebf
Simon Ehrlich <ehrlich97@gmail.com>
2024-07-14 01:42:45 +0200

    Resume page done

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Resume from "./pages/Resume";
 
 function App() {
   return (
     <div id='container'>
       <Header />
-      <Home />
+      {/* <Home /> */}
       {/* <About /> */}
+      <Resume />
       <Footer />
     </div>
   );
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,5 +1,15 @@
+import "../styles/resume.css"
+
 export default function Resume() {
     return (
-        <div></div>
+        <div className="cv-container">
+            <div className="cv">
+                <img src="/public/Screenshot 2024-07-14 at 01.27.19.png" alt="" />
+            </div>
+            <div className="cv">
+                <img src="/public/Screenshot 2024-07-14 at 01.27.40.png" alt="" />
+            </div>
+            <button className="download-button">Download CV</button>
+        </div>
     )
 }
