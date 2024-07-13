89d1825c23f0210080c06e21a00cae84d548169a
Simon Ehrlich <ehrlich97@gmail.com>
2024-07-14 01:51:00 +0200

    layout Resume fixed

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,15 +1,15 @@
-import "../styles/resume.css"
+import "../styles/resume.css";
 
 export default function Resume() {
-    return (
-        <div className="cv-container">
-            <div className="cv">
-                <img src="/public/Screenshot 2024-07-14 at 01.27.19.png" alt="" />
-            </div>
-            <div className="cv">
-                <img src="/public/Screenshot 2024-07-14 at 01.27.40.png" alt="" />
-            </div>
-            <button className="download-button">Download CV</button>
-        </div>
-    )
+  return (
+    <div className="cv-container">
+      <div className="cv">
+        <img src="/public/Screenshot 2024-07-14 at 01.27.19.png" alt="" />
+      </div>
+      <div className="cv">
+        <img src="/public/Screenshot 2024-07-14 at 01.27.40.png" alt="" />
+      </div>
+      <button className="download-button">Download CV</button>
+    </div>
+  );
 }
