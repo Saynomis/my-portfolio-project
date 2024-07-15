c6c567d4848619594f62ba3318a92566ba207aa9
Simon Ehrlich <ehrlich97@gmail.com>
2024-07-16 00:12:28 +0200

    Resume page implemented with pdfViewer

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,10 +1,48 @@
-import '../styles/resume.css'
+import { useState } from "react";
+import { Document, Page, pdfjs } from "react-pdf";
+import { version as pdfjsVersion } from "pdfjs-dist/package.json";
+import "../styles/resume.css"
+import "react-pdf/dist/Page/AnnotationLayer.css";
+import "react-pdf/dist/Page/TextLayer.css";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjsVersion}/build/pdf.worker.min.mjs`;
 
 const Resume = () => {
- return (
- <div className="cv-container">
- <iframe  src="/Resume.pdf" width="80%" height="900px" />
- </div>
- );
+  const [pageNumber, setPageNumber] = useState(1);
+  const [numPages, setNumPages] = useState(null);
+
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+  };
+
+  const goToPrevPage = () => {
+    setPageNumber((prevPageNumber) => Math.max(prevPageNumber - 1, 1));
+  };
+
+  const goToNextPage = () => {
+    setPageNumber((prevPageNumber) => Math.min(prevPageNumber + 1, numPages));
+  };
+
+  return (
+    <div className="resume-container">
+      <Document className="resume-box" file="/Resume.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+        <Page pageNumber={pageNumber} />
+      </Document>
+      <div className="btn-container">
+        <button onClick={goToPrevPage} disabled={pageNumber <= 1}>
+          Previous
+        </button>
+        <button onClick={goToNextPage} disabled={pageNumber >= numPages}>
+          Next
+        </button>
+      </div>
+      <div className="btn2-container">
+        <button>
+          Download CV
+        </button>
+      </div>
+    </div>
+  );
 };
+
 export default Resume;
