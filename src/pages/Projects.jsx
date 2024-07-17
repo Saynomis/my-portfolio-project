828800f78d2ee639d51d8cdae44556a423572c19
Simon Ehrlich <ehrlich97@gmail.com>
2024-07-17 15:00:11 +0200

    finished

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,12 @@
-import '../styles/projects.css'
+import "../styles/projects.css";
 
 export default function Projects() {
-    return (
-        <div className='container'>
-            <h1>Coming soon...</h1>
-        </div>
-    )
+  return (
+    <div className="container">
+      <h1>Coming soon...</h1>
+      <div className="progress-bar">
+        <div className="progress-bar-fill"></div>
+      </div>
+    </div>
+  );
 }
